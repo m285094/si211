efd5b3b3c361def89fd95d30a22eaa8167bff6ec
m285094 <m285094@usna.edu>
2026-02-05 15:29:03 -0500

    msg

--- proj01/Section.java
+++ proj01/Section.java
@@ -17,23 +17,4 @@ public class Section {
 
     //print section in proper format
     public String toString() { return name + " " + id + " " + time + " " + loc; }
-
-    public static void printSections(Node L, String s) {
-
-        //loop through list of sections
-        for(Node p = L; p != null; p = p.next) {
-            if(s.equals(p.data.getName())) //print if it matches target
-                System.out.println(p.data);
-        }
-    }
-
-    public static Section getSection(Node L, String s, String n) {
-
-        for(Node p = L; p != null; p = p.next) {
-            if(s.equals(p.data.getName()) && n.equals(p.data.getID()))
-                return p.data; //returns true if course name and id are in there
-        }
-
-        return null; //returns null if doesn't exist
-    }
 }
